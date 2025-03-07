7e80e6553b2344eb5e0d42e63e6d72083e48fe73
DivyaAmruthaAkula <divyaamruthaakula@gmail.com>
2025-03-07 13:52:29 +0530

    AccessNestedVal.py

--- Python Dictionary/AccessNestedVal.py
+++ Python Dictionary/AccessNestedVal.py
@@ -6,11 +6,6 @@ students={
     4:{'Name':'Ram','Branch':'Bcom'},
     5:{'Name':'Chandu','Branch':'Btech'}
 }
-'''# 1.5 Accessing values from a nested dictionary
-student_id=2
-print(f"Student {student_id} Details: {students.get(student_id, 'Not Found')}")
-print(f"Name of Student {student_id}: {students[student_id]['Name']}")
-print(f"Grade of Student {student_id}: {students[student_id]['Branch']}")'''
 n=int(input("Enter Roll_no"))
 print("Name of student:",students[n]['Name'])
 print("Branch: ",students[n]['Branch'])
